c9efe4da0a8dd3fa3d4a851e11ff81ec8b5fa4ae
joaoercmendes <joaomendesfullstack@gmail.com>
2025-03-16 17:48:41 +0000

    Changes so the month and day show up correctly in pt-PT

--- index.js
+++ index.js
@@ -5,14 +5,17 @@ const yearEl = document.getElementById('year');
 
 const date = new Date();
 
-monthNameEl.innerText = date.toLocaleString('en', {
+monthNameEl.innerText = monthNameEl.innerText = date.toLocaleString('pt-BR', {
   month: 'long'
-});
+}).charAt(0).toUpperCase() + date.toLocaleString('pt-PT', {
+  month: 'long'
+}).slice(1);
 
-
-dayNameEl.innerText = date.toLocaleString('en', {
+dayNameEl.innerText = date.toLocaleString('pt-PT', {
   weekday: 'long'
-});
+}).charAt(0).toLocaleUpperCase() + date.toLocaleString('pt-PT', {
+  weekday: 'long'
+}).slice(1);
 
 dayNumEl.innerHTML = date.getDate();
 
